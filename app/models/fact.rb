0985a4bf55997b4893826d5c055df9be619413f9
Jordan Beck <jordanfbeck@gmail.com>
2021-05-30 10:47:27 -0600

    Add updates for simplecov to gitignore and rails helper, and shoulda matchers to rails helper

--- app/models/fact.rb
+++ app/models/fact.rb
@@ -1,3 +1,9 @@
 class Fact < ApplicationRecord
+  validates_presence_of :title,
+                        :details,
+                        :lat,
+                        :lng,
+                        :state_id
+
   belongs_to :state
 end
--- spec/models/fact_spec.rb
+++ spec/models/fact_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Fact, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it { should validate_presence_of :title}
+    it { should validate_presence_of :details }
+    it { should validate_presence_of :lat }
+    it { should validate_presence_of :lng }
+    it { should validate_presence_of :state_id }
+  end
+  describe "relationships" do
+    it { should belong_to :state }
+  end
 end
