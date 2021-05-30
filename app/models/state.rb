c6a43813c05031bca9db9821b4fee3c5fd6f5873
Jordan Beck <jordanfbeck@gmail.com>
2021-05-30 10:35:06 -0600

    Adjust seed file syntax, and state_spec model test

--- app/models/state.rb
+++ app/models/state.rb
@@ -1,3 +1,9 @@
 class State < ApplicationRecord
+  validates_presence_of :name,
+                        :description,
+                        :flag_image,
+                        :captitol_name,
+                        :population
+
   has_many :facts, dependent: :destroy
 end
--- spec/models/state_spec.rb
+++ spec/models/state_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe State, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :description }
+    it { should validate_presence_of :flag_image }
+    it { should validate_presence_of :captitol_name }
+    it { should validate_presence_of :population }
+  end
+  describe "relationships" do
+    it { should have_many :facts }
+  end
 end
